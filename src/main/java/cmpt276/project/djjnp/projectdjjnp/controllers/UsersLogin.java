72f6471f0bb5d2c35559544da997f8ea018d1d8f
Huhoi <jsntruong03@gmail.com>
2023-06-26 16:02:11 -0700

    added login

--- src/main/java/cmpt276/project/djjnp/projectdjjnp/controllers/UsersLogin.java
+++ src/main/java/cmpt276/project/djjnp/projectdjjnp/controllers/UsersLogin.java
@@ -9,18 +9,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import cmpt276.project.djjnp.projectdjjnp.models.User;
 import cmpt276.project.djjnp.projectdjjnp.models.UserRepository;
 import cmpt276.project.djjnp.projectdjjnp.service.UserService;
+import cmpt276.project.djjnp.projectdjjnp.service.UserServiceImplementation;
 
 @Controller
 public class UsersLogin {
     
     @Autowired
     private UserService service;
-    
+
+    @Autowired(required = true)
+    private UserRepository userRepo;
+
+    //------------------------------------------
+    // Registration Mapping
+    //------------------------------------------
     @GetMapping("/view/register")
     public String register(Model model){
         
@@ -36,9 +44,31 @@ public class UsersLogin {
         return "view/login";
     }
 
+    //------------------------------------------
+    // Login Mapping
+    //------------------------------------------
     @GetMapping("/view/login")
     public String login(Model model){
+        User users = new User();
+        model.addAttribute("us", users);
         return "view/loginPage";
+    }
+
+    @PostMapping("/view/loginUser")
+    public String loginUser(@ModelAttribute("us") User user){
+        System.out.println(user.getEmail());
+        System.out.println(user.getPassword());
+        
+        String userEmail = user.getEmail();
+        User userData = this.userRepo.findByEmail(userEmail);
+
+        if(user.getPassword().equals(userData.getPassword())){
+            return "view/success";
+        }
+        else{
+            return"error";
+        }
+        
     }
 
 
--- src/main/java/cmpt276/project/djjnp/projectdjjnp/models/UserRepository.java
+++ src/main/java/cmpt276/project/djjnp/projectdjjnp/models/UserRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer>{
-    List<User> findByEmail(String email);
+    User findByEmail(String email);
     List<User> findByEmailAndPassword(String email, String password);
     List<User> findByUid(int uid);
 }
