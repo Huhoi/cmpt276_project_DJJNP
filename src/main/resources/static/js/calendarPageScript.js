1c3f6a45ad86d9e36c1b5a4ce0fd983398be8d10
Huhoi <jsntruong03@gmail.com>
2023-07-17 23:44:36 -0700

    added comments

--- src/main/resources/static/js/calendarPageScript.js
+++ src/main/resources/static/js/calendarPageScript.js
@@ -11,7 +11,62 @@ const backDrop = document.getElementById('modalBackDrop');
 const eventTitleInput = document.getElementById('eventTitleInput');
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+// let weatherMapping = {
+//   0: "Clear sky",
+//   1: "Mainly clear",
+//   2: "Partly cloudy",
+//   3: "Overcast",
+//   45: "Fog",
+//   48: "Depositing rime fog",
+//   51: "Drizzle: Light",
+//   53: "Drizzle: Moderate",
+//   55: "Drizzle: Dense intensity",
+//   56: "Freezing Drizzle: Light",
+//   57: "Freezing Drizzle: Dense intensity",
+//   61: "Rain: Slight",
+//   63: "Rain: Moderate",
+//   65: "Rain: Heavy intensity",
+//   66: "Freezing Rain: Light",
+//   67: "Freezing Rain: Heavy intensity",
+//   71: "Snow fall: Slight",
+//   73: "Snow fall: Moderate",
+//   75: "Snow fall: Heavy intensity",
+//   77: "Snow grains",
+//   80: "Rain showers: Slight",
+//   81: "Rain showers: Moderate",
+//   82: "Rain showers: Violent",
+//   85: "Snow showers: Slight",
+//   86: "Snow showers: Heavy",
+//   95: "Thunderstorm: Slight or moderate",
+//   96: "Thunderstorm with slight hail",
+//   99: "Thunderstorm with heavy hail"
+// };
 
+// function decodeWeatherCode(code) {
+//   return weatherMapping[code] || "Unknown";
+// }
+
+
+// async function fetchWeatherData() {
+//   try {
+//     const result = await fetch("https://api.open-meteo.com/v1/forecast?latitude=49.24&longitude=-122.98&daily=weathercode&timezone=America%2FLos_Angeles&forecast_days=16&models=gem_seamless");
+//     return await result.json();
+//   } catch (error) {
+//     console.error("Error fetching weather data:", error);
+//     return null;
+//   }
+// }
+
+
+function openModal(date) {
+  clicked = date;
+  document.getElementById('selectedDate').value = date;
+
+  const eventForDay = events.find(e => e.date === clicked);
+
+  if (eventForDay) {
+    document.getElementById('eventText').innerText = eventForDay.title;
+    deleteEventModal.style.display = 'block';
 
 
 function openModal(date) {
